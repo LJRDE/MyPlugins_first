6c179319ce69250fa812a7a37da1c570dd45fd61
LJRDE <box811226@163.com>
2025-08-02 21:20:54 +0800

    完成构建MyPlugins1.0

---
+++ src/main/java/Dim_LJR/myPlugins_first.java
@@ -0,0 +1,17 @@
+package Dim_LJR;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class myPlugins_first extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
